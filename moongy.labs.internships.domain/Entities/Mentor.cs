1a46361becc6c14d1b8206015d68a64f93f3d59b
Robi-bobi69 <devicrobi93@gmail.com>
2026-06-02 12:29:07 +0100

    zjhtf

---
+++ moongy.labs.internships.domain/Entities/Mentor.cs
@@ -0,0 +1,11 @@
+﻿namespace moongy.labs.internships.Domain.Entities
+{
+    public class Mentor
+    {
+        public int MentorId { get; set; }
+        public int OrganizationId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ moongy.labs.internships.domain/Entities/Partner.cs
@@ -0,0 +1,8 @@
+﻿namespace moongy.labs.internships.Domain.Entities
+{
+    public class Partner
+    {
+        public int PartnerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
